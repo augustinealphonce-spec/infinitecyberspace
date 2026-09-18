336acc5b5893a51a5493aa7df590b11d12b3c194
Agus <augustinealphonce838@gmail.com>
2026-09-18 15:39:44 +0300

    Delete infinitecyberspace directory

--- infinitecyberspace/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once __DIR__ . '/config.php';
-
-echo json_encode([
-    'status'  => 'ok',
-    'service' => 'Infinite Cyberspace Payments (PHP)',
-    'env'     => PESAPAL_SANDBOX ? 'sandbox' : 'live',
-]);
